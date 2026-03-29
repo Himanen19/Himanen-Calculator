a16ac27232b855b80953dc035739c5fe116de4e3
Himanen19 <himanentv@gmail.com>
2026-03-29 08:24:06 -0300

    adicionando botoes

---
+++ calculator.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const btn1 = document.getElementById('btn_1');
+const btn2 = document.getElementById('btn_2');
+const btn3 = document.getElementById('btn_3');
+const btn4 = document.getElementById('btn_4');
+const btn5 = document.getElementById('btn_5');
+const btn6 = document.getElementById('btn_6');
+const btn7 = document.getElementById('btn_7');
+const btn8 = document.getElementById('btn_8');
+const btn9 = document.getElementById('btn_9');
+const btn0 = document.getElementById('btn_0');
+const btnMultiplica = document.getElementById('btn_X');
+const btnDivide = document.getElementById('btn_/');
+const btnMais = document.getElementById('btn_+');
+const btnMenos = document.getElementById('btn_-');
+const btnPonto = document.getElementById('btn_.');
+btn1.addEventListener('click', () => console.log(`um`));
+
+btnMultiplica.addEventListener('click', () => console.log(`*`));
