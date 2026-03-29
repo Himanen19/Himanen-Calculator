aae42159ae977a85659d9a79a1335117bd241f2b
Himanen19 <himanentv@gmail.com>
2026-03-29 09:06:39 -0300

    mudacas

--- calculator.js
+++ calculator.js
@@ -1,34 +1,29 @@
 'use strict';
 
-const btn1 = document.getElementById('btn_1');
-const btn2 = document.getElementById('btn_2');
-const btn3 = document.getElementById('btn_3');
-const btn4 = document.getElementById('btn_4');
-const btn5 = document.getElementById('btn_5');
-const btn6 = document.getElementById('btn_6');
-const btn7 = document.getElementById('btn_7');
-const btn8 = document.getElementById('btn_8');
-const btn9 = document.getElementById('btn_9');
-const btn0 = document.getElementById('btn_0');
-const btnMultiplica = document.getElementById('btn_X');
-const btnDivide = document.getElementById('btn_/');
-const btnAdd = document.getElementById('btn_+');
-const btnSubt = document.getElementById('btn_-');
-const btnPonto = document.getElementById('btn_.');
+// const btn1 = document.getElementById('btn_1');
+// const btn2 = document.getElementById('btn_2');
+// const btn3 = document.getElementById('btn_3');
+// const btn4 = document.getElementById('btn_4');
+// const btn5 = document.getElementById('btn_5');
+// const btn6 = document.getElementById('btn_6');
+// const btn7 = document.getElementById('btn_7');
+// const btn8 = document.getElementById('btn_8');
+// const btn9 = document.getElementById('btn_9');
+// const btn0 = document.getElementById('btn_0');
+// const btnMultiplica = document.getElementById('btn_X');
+// const btnDivide = document.getElementById('btn_/');
+// const btnAdd = document.getElementById('btn_+');
+// const btnSubt = document.getElementById('btn_-');
+// const btnPonto = document.getElementById('btn_.');
 const display = document.getElementById('display');
 const displayTotal = document.getElementById('displayTotal');
 let currentValue = 0;
-btn1.addEventListener('click', function () {
-    showOnDisplay(1);
-});
 
 const showOnDisplay = function (digitPressed) {
     display.value += digitPressed;
 };
 
-btnAdd.addEventListener('click', function () {
-    currentValue = +display.value;
-    displayTotal.value += currentValue + '+';
+const clearDisplay = function () {
     display.value = '';
-    console.log(currentValue);
-});
+    displayTotal.value = '';
+};
