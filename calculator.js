f3ec5ba3845d65d41a9e63f06d6fab9532a14c43
Himanen19 <himanentv@gmail.com>
2026-03-29 12:38:52 -0300

    Nova UI

--- calculator.js
+++ calculator.js
@@ -22,31 +22,48 @@ const showOnDisplay = function (digitPressed) {
     display.value += digitPressed;
 };
 
+display.addEventListener('input', function () {
+    this.value = this.value.replace(/[^0-9+\-*/.]/g, '');
+    console.log(`apagado`);
+});
 const clearDisplay = function () {
     display.value = '';
     displayTotal.value = '';
-    totalMath.value = '';
 };
 
 const calculate = function () {
     const calculations = display.value;
+    if (/[a-zA-Z]/.test(calculations)) {
+        return;
+    }
     const tokens = calculations.split(/([+\-*/])/);
     let valores = tokens.map(t => (isNaN(t) ? t : Number(t)));
-    console.log(tokens);
-    console.log(valores);
     let total = 0;
+    // multiplicação e divisão primeiro
     for (let i = 0; i < valores.length; i++) {
-        if (valores[i] === '/' || valores[i] === '*' || valores[i] === '-' || valores[i] === '+') {
+        if (valores[i] === '/' || valores[i] === '*') {
             let esquerda = valores[i - 1];
             let direita = valores[i + 1];
-            total = valores[i] === '*' ? esquerda * direita : valores[i] === '/' ? esquerda / direita : valores[i] === '-' ? esquerda - direita : valores[i] === '+' ? esquerda + direita : '';
+            total = valores[i] === '*' ? esquerda * direita : esquerda / direita;
             valores.splice(i - 1, 3, total);
             console.log(valores);
 
             i--;
         }
     }
+    // depois soma e subtração
+    for (let i = 0; i < valores.length; i++) {
+        if (valores[i] === '-' || valores[i] === '+') {
+            let esquerda = valores[i - 1];
+            let direita = valores[i + 1];
+            total = valores[i] === '-' ? esquerda - direita : esquerda + direita;
+            valores.splice(i - 1, 3, total);
+            console.log(valores);
 
+            i--;
+        }
+    }
+    // att o display
     displayTotal.value = total;
     console.log(total);
 };
